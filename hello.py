556b10290bc9520ecfbcd9136588a560ac2a8be5
cowtony <cowtony@163.com>
2017-11-17 09:31:48 -0500

    'asd'

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def index():
     return "<h1>Hello heroku</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
